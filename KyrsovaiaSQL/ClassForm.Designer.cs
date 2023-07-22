f1d09381ddb9a67e219e36b62936833a8fa5a690
Kondratov2504 <kondratd19@gmail.com>
2023-07-22 16:58:56 +0500

    Добавили TestAppend

--- KyrsovaiaSQL/ClassForm.Designer.cs
+++ KyrsovaiaSQL/ClassForm.Designer.cs
@@ -35,13 +35,17 @@
             this.schoolMagazineClassDataSet = new KyrsovaiaSQL.SchoolMagazineClassDataSet();
             this.classTableAdapter = new KyrsovaiaSQL.SchoolMagazineClassDataSetTableAdapters.ClassTableAdapter();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Name = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.Id_ = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name_ = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.Age = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.classBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.schoolMagazineClassDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -85,8 +89,8 @@
             this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Id,
-            this.Name,
+            this.Id_,
+            this.Name_,
             this.Age});
             this.dataGridView2.Location = new System.Drawing.Point(12, 222);
             this.dataGridView2.Name = "dataGridView2";
@@ -96,19 +100,33 @@
             this.dataGridView2.TabIndex = 1;
             this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
-            // Id
+            // dataGridView3
             // 
-            this.Id.FillWeight = 46.75668F;
-            this.Id.HeaderText = "Ид";
-            this.Id.MinimumWidth = 6;
-            this.Id.Name = "Id";
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Name});
+            this.dataGridView3.Location = new System.Drawing.Point(415, 12);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(373, 164);
+            this.dataGridView3.TabIndex = 2;
             // 
-            // Name
+            // Id_
             // 
-            this.Name.FillWeight = 20F;
-            this.Name.HeaderText = "Имя";
-            this.Name.MinimumWidth = 15;
-            this.Name.Name = "Name";
+            this.Id_.FillWeight = 46.75668F;
+            this.Id_.HeaderText = "Ид";
+            this.Id_.MinimumWidth = 6;
+            this.Id_.Name = "Id_";
+            // 
+            // Name_
+            // 
+            this.Name_.FillWeight = 20F;
+            this.Name_.HeaderText = "Имя";
+            this.Name_.MinimumWidth = 15;
+            this.Name_.Name = "Name_";
             // 
             // Age
             // 
@@ -121,20 +139,36 @@
             this.Age.MinimumWidth = 6;
             this.Age.Name = "Age";
             // 
+            // Id
+            // 
+            this.Id.FillWeight = 64.17112F;
+            this.Id.HeaderText = "Id";
+            this.Id.MinimumWidth = 6;
+            this.Id.Name = "Id";
+            // 
+            // Name
+            // 
+            this.Name.FillWeight = 135.8289F;
+            this.Name.HeaderText = "Name";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            // 
             // ClassForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
-            //System.Windows.Forms.Control.Name = "ClassForm";
+            //this.Name = "ClassForm";
             this.Text = "Классы";
             this.Load += new System.EventHandler(this.FormLoad);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.classBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.schoolMagazineClassDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -147,8 +181,11 @@
         private SchoolMagazineClassDataSetTableAdapters.ClassTableAdapter classTableAdapter;
         private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
-        private System.Windows.Forms.DataGridViewCheckBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id_;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Name_;
         private System.Windows.Forms.DataGridViewComboBoxColumn Age;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
     }
 }
--- KyrsovaiaSQL/ClassForm.cs
+++ KyrsovaiaSQL/ClassForm.cs
@@ -12,6 +12,11 @@ namespace KyrsovaiaSQL
 {
     public partial class ClassForm : Form
     {
+        private string[] ages =
+        {
+            "20","21","22"
+        };
+
         public ClassForm()
         {
             InitializeComponent();
@@ -21,18 +26,31 @@ namespace KyrsovaiaSQL
         {
             // TODO: данная строка кода позволяет загрузить данные в таблицу "schoolMagazineClassDataSet.Class". При необходимости она может быть перемещена или удалена.
             this.classTableAdapter.Fill(this.schoolMagazineClassDataSet.Class);
-            //dataGridView2.
-            string[] row0 = { "11/22/1968","true","21"};
-            
 
-            dataGridView2.Rows.Add(row0);
-            dataGridView2.Rows.Add("1",true,"21");
+            /*dataGridView2.Rows.Add(row0);
+            dataGridView2.Rows.Add("1",true,"21");*/
+            TestAppend();
 
+
+        }
+
+        private void TestAppend()
+        {
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < dataGridView2.ColumnCount; i++)
+            {
+                int id = rnd.Next(1, 1000);
+                bool name = Convert.ToBoolean(rnd.Next(0, 2));
+                string age = ages[rnd.Next(0, 3)];
+                dataGridView2.Rows.Add(id.ToString(), name, age);
+            }
         }
 
         private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+             
         }
     }
 }
